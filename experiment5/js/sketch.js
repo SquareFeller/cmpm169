d5017f423a2000836c9980f1cf3bbb88e750073b
Isai Rincon <Rinconisai@gmail.com>
2025-02-09 22:31:59 -0800

    film grain effect

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -21,7 +21,15 @@ let mouseScroll = 0;
 let hasplayed = false;
 let train;
 let cam; 
-//let mouse_down = 0;
+
+/*
+Using texture image example code from the p5.grain library by user meezwhite -- https://github.com/meezwhite/p5.grain/blob/main/examples/texture-overlay-outside/sketch.js
+*/
+
+let textureImage;
+let tW, tH;
+let textureElement;
+
 
 function preload(){
   soundFormats('mp3');
@@ -35,6 +43,9 @@ function preload(){
 
   //train model by z-lengyel -- https://www.cgtrader.com/products/mav-40-steam-locomotive-tank-engine
   train = loadModel('mav_40.obj','.obj');
+
+  //texture image by luis_molinero -- https://www.freepik.com/free-photo/rough-textured-wall_1198446.htm#fromView=image_search_similar&page=1&position=9&uuid=a87639a7-b31b-4b39-b572-de775eebb5c2&query=Film+Grain+Texture
+  textureImage = loadImage('./js/texture.jpg');
 }
 
 
@@ -43,7 +54,10 @@ function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
   console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());  
+  p5grain.setup();
+  tW = textureImage.width;
+  tH = textureImage.height;
   // redrawCanvas(); // Redraw everything based on new size
 }
 
@@ -60,50 +74,43 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+
+  textureElement = document.querySelector('#texture');
+
+  //cam setup 
   cam = createCamera();
- cam.setPosition(683.2680712547467, 183.8614227989123, 7289.548672281939);
- //cam.setPosition(-291.6911247929137, 160.60902220871282, 6420.088523244931);
-
+  cam.setPosition(683.2680712547467, 183.8614227989123, 7289.548672281939);
   cam.lookAt(200.37541618027217, -137.3225618638527, 359.57758027678346);
-  //cam.lookAt(-76.13160623632469, 94.93953006125913, 460.162139298443);
-  //cam.roll(0.5);
 
+  //sound setup
   trainSound.loop();
   trainSound.amp(10);
   trainSound.setVolume(0);
-  crowdSound.amp(25);
+  crowdSound.amp(45);
   crowdSound.setVolume(0);
   trainSound.play();
 }
 
 function draw() {
-  background(220);
-  //cam.roll(0.01);
+  console.log(displayWidth * pixelDensity() + "," + displayHeight * pixelDensity());
+  background(65);
+  
   orbitControl();
-  //let b = box(30, 10, 10);
-  //normalMaterial();
   let t = model(train);
-  //cam.roll(190);
   if(trainSound.getVolume() > 0.95 && !hasplayed){
     hasplayed = true;
     crowdSound.setVolume(1);
     crowdSound.play();
   }
-  // cam x: -291.6911247929137 cam y: 160.60902220871282 cam z: 6420.088523244931
-//sketch.js:88 look at x: -76.13160623632469 look at y: 94.93953006125913 look at z: 460.162139298443
-/*
-cam x: 683.2680712547467 cam y: 183.8614227989123 cam z: 7289.548672281939
-sketch.js:93 look at x: 200.37541618027217 look at y: -137.3225618638527 look at z: 359.57758027678346
-*/ 
-//furthest z:  cam z: 7289.54867228193
-// closest z: 439.2988991833581
- // console.log(" cam z: " + cam.eyeZ);
-//  console.log(" look at z: " + cam.centerZ);
+
+  textureAnimate(textureElement);
 }
 
 function mousePressed() {
   userStartAudio();
 }
+
+
 
 function mouseWheel(event){
   console.log("event delta: " + event.delta);
@@ -114,25 +121,16 @@ function mouseWheel(event){
     }else{
       if(trainSound.getVolume()>=1){
         trainSound.setVolume(1);
-      //  crowdSound.play();
-        //trainSound.amp(15);
       }else{
         trainSound.setVolume(mouseScroll+=0.05);
       }
     }
-    console.log(mouseScroll);
-    //audio file vol go up
   }else{
     if(mouseScroll > 0.099){
-      // if(cam.eyeZ > 439.2988991833581){
          trainSound.setVolume(mouseScroll-=0.04);
     }else{
           mouseScroll = 0;
           trainSound.setVolume(mouseScroll);
          }
-         console.log(mouseScroll);
-         
-         //audio file vol go down
-    //if(trainSound.getVolume() < 0){
   }
 }
